a14b611c2c9a9ab1c9fbc373319b5cedbf96724d
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-03-20 17:19:44 +1100

    contact page added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 import Container from "@/components/Container";
+import MainContainer from "@/components/MainContainer";
+import Image from "next/image";
 
 const ContactPage = () => {
   return (
     <Container className="h-[80vh]">
-      <div>Contact Page</div>
+      <MainContainer className="w-[1220px] h-full grid grid-cols-2">
+        <div className="h-full grid items-center">
+          <Image src="/contact.png" alt="contact" width={500} height={500} />
+        </div>
+        <div className="w-full h-full">
+          <form
+            action=""
+            className="w-full h-full flex flex-col justify-center gap-4"
+          >
+            <input
+              type="text"
+              placeholder="Name and Surname"
+              className="p-4 bg-blue-950 w-full rounded-md"
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="p-4 bg-blue-950 w-full rounded-md"
+            />
+            <input
+              type="text"
+              placeholder="Phone Number (Optional)"
+              className="p-4 bg-blue-950 w-full rounded-md"
+            />
+            <textarea
+              name=""
+              id=""
+              placeholder="Message"
+              className="p-4 bg-blue-950 h-60 w-full rounded-md"
+            ></textarea>
+            <button
+              type="submit"
+              className="w-full py-4 bg-blue-500 hover:bg-blue-700 transition-colors rounded-md"
+            >
+              Send
+            </button>
+          </form>
+        </div>
+      </MainContainer>
     </Container>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Container from "./Container";
+import MainContainer from "./MainContainer";
 
 const Footer = () => {
   return (
-    <Container>
-      <footer className="w-[1220px]">Footer</footer>
+    <Container className="h-[10vh] pb-4">
+      <MainContainer className="w-[1220px] h-full">
+        <footer className="w-full h-full flex items-end justify-between">
+          <p className="font-semibold">cj@dev</p>
+          <p>
+            Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+          </p>
+        </footer>
+      </MainContainer>
     </Container>
   );
 };
